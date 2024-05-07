8ba896d28f8a64fddaefb64e5bdd3df962134888
Arkkul <104141207+Arkkul@users.noreply.github.com>
2024-05-07 20:31:54 +0300

    добавил скрипты подбора объектов

---
+++ HorrorGame/Assets/Scripts/PickingObject.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PickingObject : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void PickUp(Transform parent, float holdDistance)
+    {
+        transform.SetParent(parent);
+        transform.localPosition = new Vector3(0f, 0f, holdDistance);
+
+        _rigidbody.isKinematic = true;
+    }
+
+    public void Throw(Vector3 force)
+    {
+        transform.SetParent(null);
+        _rigidbody.isKinematic = true;
+
+        if(force.sqrMagnitude > 0f)
+        {
+            _rigidbody.AddForce(force);
+        }
+    }
+}
