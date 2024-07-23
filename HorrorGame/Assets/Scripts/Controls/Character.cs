41dca675c386afba9891a6ded295f9761e0e7530
Arkkul <104141207+Arkkul@users.noreply.github.com>
2024-07-23 15:47:21 +0300

    return fix

--- HorrorGame/Assets/Scripts/Controls/Character.cs
+++ HorrorGame/Assets/Scripts/Controls/Character.cs
@@ -110,7 +110,7 @@ public class Character : MonoBehaviour, IControllable
         if (_direction != Vector3.zero)
         {
             _isMove = true;
-            _characterController.Move(_direction * _speed * Time.fixedDeltaTime);
+            _characterController.Move(_direction * _speed * Time.deltaTime);
         }
         else
         {
