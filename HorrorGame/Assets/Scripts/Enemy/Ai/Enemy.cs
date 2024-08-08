7b24d13045d46a28b784f035ae78113b066531a0
Tvenx <128207591+Tvenx@users.noreply.github.com>
2024-08-08 11:56:51 +0300

    enemy template
    Добавил шаблон врага мб будет базой  для остальных

---
+++ HorrorGame/Assets/Scripts/Enemy/Ai/Enemy.cs
@@ -0,0 +1,192 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]private NavMeshAgent _navAgent;
+
+    [SerializeField] private List<Transform> destinations;
+
+    [SerializeField] private Animator aiAnim;
+
+    [Header("-----Properties-----")]
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float chaseSpeed;
+
+    [SerializeField] private float minIdleTime;
+    [SerializeField] private float maxIdleTime;
+    [SerializeField] private float idleTime;
+
+    [SerializeField] private float sightDistance;
+    [SerializeField] private float catchDistance;
+
+    [SerializeField] private float chaseTime;
+    [SerializeField] private float minChaseTime;
+    [SerializeField] private float maxChaseTime;
+
+    [SerializeField] private float jumpscareTime;
+
+    [SerializeField] private Transform player;
+
+    private bool walking;
+    private bool chasing;
+
+    private int randNum;
+
+    private Transform currentDest;
+    private Vector3 dest;
+    // public string deathScene;
+
+    [Header("-----View-----")]
+    [SerializeField] private float viewRadius;
+    [Range(0, 360)]
+    [SerializeField] private float viewAngle;
+    [SerializeField] private LayerMask targetMask;
+    [SerializeField] private LayerMask obstacleMask;
+    [SerializeField] private List<Transform> visibleTargets = new List<Transform>();
+
+    void Start()
+    {
+        walking = true;
+        StartCoroutine("FindTargetsWithDelay", 0.2f);
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+    }
+    void Update()
+    {
+        if (chasing == true)
+        {
+            Chase();
+        }
+        if (walking == true)
+        {
+            Walk();
+        }
+    }
+    IEnumerator stayIdle()
+    {
+        idleTime = Random.Range(minIdleTime, maxIdleTime);
+        yield return new WaitForSeconds(idleTime);
+        walking = true;
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+    }
+    IEnumerator chaseRoutine()
+    {
+        chaseTime = Random.Range(minChaseTime, maxChaseTime);
+        yield return new WaitForSeconds(chaseTime);
+        walking = true;
+        chasing = false;
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+    }
+    IEnumerator deathRoutine()
+    {
+        yield return new WaitForSeconds(jumpscareTime);
+        //SceneManager.LoadScene(deathScene);
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    private void Walk()
+    {
+        dest = currentDest.position;
+        _navAgent.destination = dest;
+        _navAgent.speed = walkSpeed;
+        aiAnim.ResetTrigger("sprint");
+        aiAnim.ResetTrigger("idle");
+        aiAnim.SetTrigger("walk");
+        if (_navAgent.remainingDistance <= _navAgent.stoppingDistance)
+        {
+            aiAnim.ResetTrigger("sprint");
+            aiAnim.ResetTrigger("walk");
+            aiAnim.SetTrigger("idle");
+            _navAgent.speed = 0;
+            StopCoroutine("stayIdle");
+            StartCoroutine("stayIdle");
+            walking = false;
+        }
+    }
+
+    public void Chase()
+    {
+        dest = player.position;
+        _navAgent.destination = dest;
+        _navAgent.speed = chaseSpeed;
+        aiAnim.ResetTrigger("walk");
+        aiAnim.ResetTrigger("idle");
+        aiAnim.SetTrigger("sprint");
+        float distance = Vector3.Distance(player.position, _navAgent.transform.position);
+        if (distance <= catchDistance)
+        {
+            player.gameObject.SetActive(false);
+            aiAnim.ResetTrigger("walk");
+            aiAnim.ResetTrigger("idle");
+            aiAnim.ResetTrigger("sprint");
+            aiAnim.SetTrigger("jumpscare");
+            StartCoroutine(deathRoutine());
+            chasing = false;
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                    if (target.CompareTag("Player"))
+                    {
+                        walking = false;
+                        StopCoroutine("stayIdle");
+                        StopCoroutine("chaseRoutine");
+                        StartCoroutine("chaseRoutine");
+                        chasing = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, viewRadius);
+
+        Vector3 viewAngleA = DirFromAngle(-viewAngle / 2, false);
+        Vector3 viewAngleB = DirFromAngle(viewAngle / 2, false);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + viewAngleA * viewRadius);
+        Gizmos.DrawLine(transform.position, transform.position + viewAngleB * viewRadius);
+    }
+}
